3b1d4a5e1f072dc5c7728b90c227602da9b3a018
Istiak <mhistiak2@gmail.com>
2024-09-26 18:04:55 +0600

    Wishlist delete success

--- src/controllers/WishListController.js
+++ src/controllers/WishListController.js
@@ -27,21 +27,40 @@ export async function createWish(req, res, next) {
   }
 }
 
-
 export async function getWishList(req, res, next) {
   try {
     const { userId } = req.headers;
-
-
     const data = {
       userID: userId,
     };
 
-    const wishList = await WishListModel.find(data);
-   
+    const wishList = await WishListModel.find(data).populate({
+      path: "productID",
+      select: "-createdAt -updatedAt -_id",
+    });
     res.json({
       type: "Success",
       data: wishList,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function deleteWish(req, res, next) {
+  try {
+    const { userId } = req.headers;
+    const { productID } = req.params;
+    const query = {
+      userID: userId,
+      productID,
+    };
+    console.log(query);
+
+    await WishListModel.deleteOne(query);
+    res.json({
+      type: "Success",
+      message: "Successfully deleted",
     });
   } catch (error) {
     next(error);
